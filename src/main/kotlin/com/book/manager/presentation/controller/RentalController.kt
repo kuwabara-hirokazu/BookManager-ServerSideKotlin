1d2357718b1f799b30acf9e632cc65b7002d8051
h.kuwabara <h.kuwabara1103@gmail.com>
2022-07-23 12:36:03 +0900

    返却処理を追加

--- src/main/kotlin/com/book/manager/presentation/controller/RentalController.kt
+++ src/main/kotlin/com/book/manager/presentation/controller/RentalController.kt
@@ -5,6 +5,8 @@ import com.book.manager.application.service.security.BookManagerUserDetails
 import com.book.manager.presentation.form.RentalStartRequest
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -22,5 +24,11 @@ class RentalController(
         // SecurityContextHolderの中に認証したユーザーの情報が保持されている
         val user = SecurityContextHolder.getContext().authentication.principal as BookManagerUserDetails
         rentalService.startRental(request.bookId, user.id)
+    }
+
+    @DeleteMapping("/end/{book_id}")
+    fun endRental(@PathVariable("book_id") bookId: Long) {
+        val user = SecurityContextHolder.getContext().authentication.principal as BookManagerUserDetails
+        rentalService.endRental(bookId, user.id)
     }
 }
