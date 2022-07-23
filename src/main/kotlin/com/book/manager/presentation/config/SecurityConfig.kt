d6369d7efc34b88a65152ee678ff2af8055a2bd3
h.kuwabara <h.kuwabara1103@gmail.com>
2022-07-23 10:58:48 +0900

    認証後の各ハンドラーを実装

--- src/main/kotlin/com/book/manager/presentation/config/SecurityConfig.kt
+++ src/main/kotlin/com/book/manager/presentation/config/SecurityConfig.kt
@@ -1,6 +1,12 @@
 package com.book.manager.presentation.config
 
+import com.book.manager.application.service.AuthenticationService
+import com.book.manager.application.service.security.BookManagerUserDetailsService
 import com.book.manager.domain.enum.RoleType
+import com.book.manager.presentation.handler.BookManagerAccessDeniedHandler
+import com.book.manager.presentation.handler.BookManagerAuthenticationEntryPoint
+import com.book.manager.presentation.handler.BookManagerAuthenticationFailureHandler
+import com.book.manager.presentation.handler.BookManagerAuthenticationSuccessHandler
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
---
+++ src/main/kotlin/com/book/manager/presentation/handler/BookManagerAuthenticationSuccessHandler.kt
@@ -0,0 +1,17 @@
+package com.book.manager.presentation.handler
+
+import org.springframework.security.core.Authentication
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+// ユーザー認証に成功した場合の処理
+class BookManagerAuthenticationSuccessHandler : AuthenticationSuccessHandler {
+    override fun onAuthenticationSuccess(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        authentication: Authentication
+    ) {
+        response.status = HttpServletResponse.SC_OK  // 200
+    }
+}
